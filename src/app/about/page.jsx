a94ad2e71e564fee2c9ee98fa672fc6d9d8fa065
opeyemi <opeyemiodunayo2018@gmail.com>
2024-12-21 16:52:16 +0100

    about page added

---
+++ src/app/about/page.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const about = () => {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">about </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae modi
+        magnam consectetur voluptas iure? Vero odio ipsam dolor omnis
+        laboriosam, odit laborum perspiciatis! Totam animi velit unde soluta!
+        Tempora, dolor!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae modi
+        magnam consectetur voluptas iure? Vero odio ipsam dolor omnis
+        laboriosam, odit laborum perspiciatis! Totam animi velit unde soluta!
+        Tempora, dolor!
+      </p>
+    </div>
+  );
+};
+
+export default about;
